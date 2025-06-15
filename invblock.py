9099b90673e8925ef1fd34ac75676193a24341e3
jj8127 <50429922+jj8127@users.noreply.github.com>
2025-06-15 23:20:16 +0900

    Update invblock.py

--- invblock.py
+++ invblock.py
@@ -1,8 +1,11 @@
+# invblock.py (수정 완료된 코드)
+
 from math import exp
 import torch
 import torch.nn as nn
 # FloatFunctional을 임포트하여 양자화된 텐서 연산을 지원하도록 합니다.
 from torch.ao.nn.quantized import FloatFunctional
+
 import config as c
 from rrdb_denselayer import ResidualDenseBlock_out
 
@@ -21,8 +24,8 @@ class INV_block(nn.Module):
         self.f = subnet_constructor(self.split_len2, self.split_len1)
 
         # 양자화된 텐서 간의 연산을 위한 기능(functional) 래퍼(wrapper)를 인스턴스화합니다.
-        self.add_func = FloatFunctional()
-        self.mul_func = FloatFunctional()
+        # 덧셈, 곱셈, 연결(concatenation)에 사용합니다.
+        self.functional = FloatFunctional()
 
     def e(self, s):
         # 이 부분은 dequantize -> 연산 -> quantize가 필요할 수 있으나, 일단 유지합니다.
@@ -32,29 +35,37 @@ class INV_block(nn.Module):
         x1, x2 = (x.narrow(1, 0, self.split_len1),
                   x.narrow(1, self.split_len1, self.split_len2))
 
-        if not rev:
+        if not rev: # 순방향 (Concealing)
             t2 = self.f(x2)
-            # y1 = x1 + t2  <- 기존 코드
+            # y1 = x1 + t2
             # FloatFunctional을 사용하여 양자화된 텐서 덧셈을 수행합니다.
-            y1 = self.add_func.add(x1, t2)
+            y1 = self.functional.add(x1, t2)
             
             s1, t1 = self.r(y1), self.y(y1)
             
-            # y2 = self.e(s1) * x2 + t1 <- 기존 코드
+            # y2 = self.e(s1) * x2 + t1
             # 곱셈과 덧셈 또한 FloatFunctional을 사용합니다.
-            y2_scaled = self.mul_func.mul(self.e(s1), x2)
-            y2 = self.add_func.add(y2_scaled, t1)
+            y2_scaled = self.functional.mul(self.e(s1), x2)
+            y2 = self.functional.add(y2_scaled, t1)
+            
+            # 최종 결과를 cat으로 합칩니다.
+            out = self.functional.cat([y1, y2], 1)
 
-        else: # 역방향(backward) 경로도 동일하게 수정합니다.
+        else: # 역방향 (Revealing)
             s1, t1 = self.r(x1), self.y(x1)
             
-            # y2 = (x2 - t1) / self.e(s1) <- 기존 코드
-            y2_sub = self.add_func.sub(x2, t1)
-            y2 = self.mul_func.div(y2_sub, self.e(s1))
+            # [수정] FloatFunctional에 없는 div, sub 대신 일반 연산자 사용
+            # y2 = (x2 - t1) / self.e(s1)
+            y2_sub = x2 - t1
+            y2 = y2_sub / self.e(s1)
 
             t2 = self.f(y2)
             
-            # y1 = (x1 - t2) <- 기존 코드
-            y1 = self.add_func.sub(x1, t2)
+            # [수정] FloatFunctional에 없는 sub 대신 일반 연산자 사용
+            # y1 = x1 - t2
+            y1 = x1 - t2
 
-        return self.add_func.cat((y1, y2), 1)
+            # 최종 결과를 cat으로 합칩니다.
+            out = self.functional.cat([y1, y2], 1)
+            
+        return out
